3438efeef50afca163bfd1058df595e924163743
Don Moses <dmoses@upei.ca>
2015-04-24 11:04:51 -0300

    update the moodle block
    theme provides a narrow left column. Adjusted and updated chat widget
    to fit, added library hours and room booking link, removed plagiarism
    quiz link.

--- library/block_library.php
+++ library/block_library.php
@@ -54,9 +54,6 @@ class block_library extends block_base {
     $this->config->text=$CFG->configcontent;
 		$this->content->text = $this->config->text;
 		$this->content->text .=getRSS("http://resources.library.upei.ca/dbofdbs/courseDBRssFeed.php?course=$COURSE->idnumber");
-		//$this->content->footer = "<div style='text-align: center;'> <a href='http://www.upei.ca/library' target='_blank'>Robertson Library</a></div>";
-		//$this->content->footer='<br><form action="http://www.oxfordreference.com.rlproxy.upei.ca/views/SEARCH_RESULTS.html" method="get" target="_blank"><input name="category" value="t62" type="hidden" /><input name="scope" value="book" type="hidden" /><input name="index" value="default" type="hidden" /><input name="q" type="text" />
-    //<input value="Define This" type="submit" /></form>';
 		$this->content->text .=getStaticStuff();
     
         return $this->content;
@@ -86,17 +83,21 @@ function getRSS($rssURL) {
 }
 function getStaticStuff(){
 	$craftyLink = <<<EOF
-    <!-- Place this div in your web page where you want your chat widget to appear. -->
-    <div class="needs-js">JavaScript disabled or chat unavailable.</div>
+	
+<div class="textout window">JavaScript disabled or chat unavailable.</div>
 
-    <!-- Place this script as near to the end of your BODY as possible. -->
-    <script type="text/javascript">
-      (function() {
-        var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-        x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?235";
-        var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-      })();
-    </script>
+<div class="textin window">Chat Is Unavailable</div>
+</div>
+</div>
+<!-- Place this script as near to the end of your BODY as possible. --><script type="text/javascript">
+  (function() {
+    var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
+    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "ca.libraryh3lp.com/js/libraryh3lp.js?50411";
+    var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
+  })();
+</script>  
+</div>
+
 EOF;
 	// this is the previous version of the libraryh3lp code.
 	/* .= "<script src=\"https://ca.libraryh3lp.com/js/libraryh3lp.js?multi\" type=\"text/javascript\"></script>
@@ -105,9 +106,10 @@ EOF;
   <div class=\"libraryh3lp\" style=\"display: none\">Library ASK US is currently offline. Please <a href=\"http://library.upei.ca/node/527\">check our other contact options.</a> </div>
   <div><br /></div>" ;
        */
-    $craftyLink .= "<div  style=\"margin-bottom: 0; padding-bottom: 0;\"><a href='http://library.upei.ca/' target='_blank'>Library Homepage</a><br>";
-	$craftyLink .= "<a href = 'http://resources.library.upei.ca/plagiarism/index.htm' target='_blank'>Avoiding Plagiarism</a>";
-	$craftyLink .='<form action="http://islandpines.roblib.upei.ca/opac/en-US/skin/roblib/xml/rresult.xml" method="get" target="_blank"><input name="rt" value="keyword" type="hidden" /><input name="tp" value="keyword" type="hidden" /><input name="t" size="17" value="Search Library Catalogue..." onblur="this.value = this.value || this.defaultValue; this.style.color = \'#999\';" type="text" onFocus="this.value=\'\'" style="color: rgb(153, 153, 153);"/><input value="Go" type="submit" /></form></div><br />';
+    $craftyLink .= "<div  style=\"margin-bottom: 0; padding-bottom: 0;\"><a href='http://library.upei.ca/' target='_blank'>Library Homepage</a><br/>";
+	$craftyLink .= "<a href= 'http://library.upei.ca/hours' target='_blank'>Library Hours</a><br/>";
+	$craftyLink .= "<a href= 'https://rooms.library.upei.ca' target='_blank'>Book A Room</a><br/>";
+	$craftyLink .='<!-- EBSCOhost Custom Search Box Begins --><script src="http://support.epnet.com/eit/scripts/ebscohostsearch.js" type="text/javascript"></script><style type="text/css">.choose-db-list{ list-style-type:none;padding:0;margin:0px 0 0 0;font-family:Verdana,Arial,Helvetica,sans-serif;font-size:9pt;width:160px; }		.choose-db-check{ width:0px;float:left;padding-left:0px;padding-top:0px; }		.choose-db-detail{ margin-left:0px;border-left:solid 1px #E7E7E7;padding:0px 0px 0px 0px;line-height:1.4em; }		.summary { background-color:#1D5DA7;color:#FFFFFF;border:solid 1px #1D5DA7; }		.one { background-color: #FFFFFF;border:solid 1px #E7E7E7;border-top:solid 1px #FFFFFF; }		.two { background-color: #F5F5F5;border:solid 1px #E7E7E7;border-top:solid 1px #FFFFFF; }		.selected { background-color: #E0EFF7;border:solid 1px #E7E7E7;border-top:solid 1px #FFFFFF; }		#ebscohostCustomSearchBox #disciplineBlock { width:auto; }		#ebscohostCustomSearchBox .limiter { float:left;margin:0;padding:0;width:50%; }		#ebscohostsearchtext { width: 150px; }		#ebscohostsearchtext.edspub { width: 150px; }				.ebscohost-search-button.edspub {			border: 1px solid #156619;			padding: 0px 0px !important;			border-radius: 0px;			color: #fff;			font-weight: bold;			background-color: #156619;		}				.ebscohost-title.edspub {			color: #1c7020;			font-weight: bold;		}</style><form action="" id="ebscohostCustomSearchBox" method="post" onsubmit="return ebscoHostSearchGo(this);" style="width:150px; overflow:auto;"><input id="ebscohostwindow" name="ebscohostwindow" type="hidden" value="1" /> <input id="ebscohosturl" name="ebscohosturl" type="hidden" value="http://proxy.library.upei.ca/login?url=http://search.ebscohost.com/login.aspx?direct=true&amp;site=eds-live&amp;scope=site&amp;type=0&amp;mode=bool&amp;lang=en&amp;authtype=ip" /> <input id="ebscohostsearchsrc" name="ebscohostsearchsrc" type="hidden" value="db" /> <input id="ebscohostsearchmode" name="ebscohostsearchmode" type="hidden" value="+" /> <input id="ebscohostkeywords" name="ebscohostkeywords" type="hidden" value="" /><div style="height:80px; width:150px; font-family:Verdana,Arial,Helvetica,sans-serif;font-size:9pt; color:#353535;"><div style="padding-top:0px;padding-left:0px;"><div><input id="ebscohostsearchtext" name="ebscohostsearchtext" size="23" style="font-size:9pt;padding-left:0px;margin-left:0px;" type="text" value="OneSearch" /> <input class="ebscohost-search-button " style="font-size:9pt;padding-left:0px;" type="submit" value="Go" /><div id="guidedFieldSelectors"><input checked="checked" class="radio" id="guidedField_0" name="searchFieldSelector" type="hidden" value="" /></div></div></div></div>';
 
 	return $craftyLink;
 }
